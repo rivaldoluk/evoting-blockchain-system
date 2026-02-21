909c8d97827405dcd9c53bdee00056c57fb55246
rivaldoluk <rivaldolukman11@gmail.com>
2026-02-21 13:50:09 +0700

    update url

--- frontend/public/dashboard/dashboard.js
+++ frontend/public/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://0469-103-129-24-34.ngrok-free.app';
+const BACKEND_URL = 'https://5152-103-129-24-89.ngrok-free.app';
 const NGROK_HEADERS = {
     "ngrok-skip-browser-warning": "69420"
 };
