a1f3dc7e1a4ea9f6ea77ebef8c0f5ad30cbe9bc5
rivaldoluk <rivaldolukman11@gmail.com>
2026-04-13 17:06:20 +0700

    update url

--- frontend/public/admin/admin.js
+++ frontend/public/admin/admin.js
@@ -1,7 +1,7 @@
 /**
  * KONFIGURASI GLOBAL
  */
-const BACKEND_URL = 'https://9c01-103-129-24-83.ngrok-free.app';
+const BACKEND_URL = 'https://3ff8-93-185-162-8.ngrok-free.app';
 const NGROK_HEADERS = {
     "ngrok-skip-browser-warning": "69420"
 };
