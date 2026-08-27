e10f2202e4bba0ceca4747481ff60900281b0df7
rivaldoluk <rivaldolukman11@gmail.com>
2026-08-27 23:09:25 +0700

    update url

--- frontend/public/admin/admin.js
+++ frontend/public/admin/admin.js
@@ -1,7 +1,7 @@
 /**
  * KONFIGURASI GLOBAL
  */
-const BACKEND_URL = 'https://c6c0-172-216-170-152.ngrok-free.app';
+const BACKEND_URL = 'https://d3da-172-216-170-152.ngrok-free.app';
 const NGROK_HEADERS = {
     "ngrok-skip-browser-warning": "69420"
 };
--- frontend/public/dashboard/dashboard.js
+++ frontend/public/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://c6c0-172-216-170-152.ngrok-free.app';
+const BACKEND_URL = 'https://d3da-172-216-170-152.ngrok-free.app';
 const NGROK_HEADERS = {
     "ngrok-skip-browser-warning": "69420"
 };
